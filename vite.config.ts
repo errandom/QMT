edc0feda23b2e1b34fb685c5c5a70b49c8d662e1
errandom <55241350+errandom@users.noreply.github.com>
2025-11-23 18:14:15 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@
 import { defineConfig } from 'vite';
 import path from 'path';
 
-// Build the client from /client and emit into /client/dist.
-// Your package.json script `copy-client` moves these assets into /dist/.
-export defaultexport default defineConfig({
+// Build client from /client to /client/dist.
+// Your package.json "copy-client" script moves these assets into /dist/.
+export default defineConfig({
   root: path.resolve(__dirname, 'client'),
   build: {
     outDir: 'dist',
     emptyOutDir: true
   }
+});
