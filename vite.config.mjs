b5c90c1cfcd5de531c6860322ba3d9098caa8f63
errandom <55241350+errandom@users.noreply.github.com>
2025-11-28 17:40:00 +0100

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+      alias: {
+        '@github/spark': '@github/spark/dist/index.js'
+      }
+    },
   root: '.',
   base: '/',
   build: {
