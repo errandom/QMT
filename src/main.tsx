f3e831a5aad3c4436f9ec6b412bbe3e06776ee54
errandom <55241350+errandom@users.noreply.github.com>
2025-11-28 17:58:24 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import { ErrorBoundary } from "react-error-boundary";
-import "@github/spark"
 
 import App from './App.tsx'
 import { ErrorFallback } from './ErrorFallback.tsx'
